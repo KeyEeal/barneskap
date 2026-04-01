e91e9ddff3425e5ec911ec69280e134d8ba61c33
Jurgen <jbrettschneider03@gmail.com>
2026-04-01 15:59:12 +0200

    Fix router base path

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,12 @@
 import ReactDOM from "react-dom/client";
 import { StrictMode } from "react";
 import App from "./App.tsx";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter basename="/barneskap">
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>,
 );
